94f795992208ee7f2f5ee2be303bd35cbbbf7e81
yonathan rizky <jrizky.jonathan@gmail.com>
2019-05-17 11:49:08 +0700

    Add button Pembelian

--- application/views/transaksi/pembelian.php
+++ application/views/transaksi/pembelian.php
@@ -4,7 +4,6 @@
     <?= $this->session->flashdata('message');?>    
     </div>
     <?= validation_errors(); ?>
-    <a href="#" class="btn btn-primary mb-3" data-toggle="modal" data-target="#exampleModal">New Transaction</a>
     <a href="<?= base_url();?>transaksi/beli" class="btn btn-primary mb-3">New Transaction</a>
 <table id="example" class="table table-striped table-bordered" style="width:100%">
         <thead>
@@ -66,52 +65,7 @@
 </div>
 
 
-    <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Add New Pembelian</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <form name="hitungRateBeli" action="<?= base_url('transaksi/pembelian')?>" method="POST">
-        <div class="form-group">
-            <label for="exampleFormControlInput1">Customer</label>
-            <input type="text" class="form-control" id="customer" name="customer" placeholder="Customer">
-            <input type="text" value="<?= $kode; ?>" hidden name="kode">
-            <?= form_error('customer','<small class="text-danger pl-3">','</small>'); ?>
-        </div>
-        <div class="form-group">
-            <label for="exampleFormControlInput1">Valas</label>
-            <select name="valas" class="form-control">
-            <option value="">Select Menu</option>
-            <?php foreach ($valas as $v) : ?> 
-            <option value="<?= $v['Id_valas'] ?>"><?= $v['valas'] ?></option>
-            <?php endforeach; ?>
-            </select>
-            <?= form_error('valas','<small class="text-danger pl-3">','</small>'); ?>
-        </div>
-        <div class="form-group">
-            <label for="exampleFormControlInput1">Rate Valas</label>
-            <input type="number" class="form-control" id="rate_valas" name="rate_valas" placeholder="Rate Valas">
-            <?= form_error('rate_valas','<small class="text-danger pl-3">','</small>'); ?>
-        </div>
-        <div class="form-group">
-            <label for="exampleFormControlInput1">Jumlah</label>
-            <input type="number" class="form-control" id="jumlah" name="jumlah" placeholder="Jumlah">
-            <?= form_error('jumlah','<small class="text-danger pl-3">','</small>'); ?>
-        </div>
-        <div class="form-group">
-            <label for="exampleFormControlInput1">Total</label>
-            <input type="number" class="form-control" id="total" placeholder="Total" name="total" readonly >
-            <?= form_error('total','<small class="text-danger pl-3">','</small>'); ?>
-        </div>
-        <input type=button name=submit onClick="hitungBeli()" class="btn btn-primary" value="Hitung">
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
-    </div>
+   
   </div>
 </div>
 

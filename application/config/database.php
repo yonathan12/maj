e43314caf621e4b05a13b8a8e7fd024e00b773b5
Yonathan Rizky Nathanael <jrizky.jonathan@gmail.com>
2020-05-07 09:48:36 +0700

    07052020

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '202.52.146.117',
-	'username' => 'yonathan_rizky',
-	'password' => 'Naelyaya2012',
-	'database' => 'yonathan_maj',
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'maj',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
